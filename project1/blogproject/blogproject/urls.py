0dc44f8b5e11f62141d8da2fb10430b966789366
ayato <ayato.211210.@gmail.com>
2025-12-23 10:23:13 +0900

    NEW

--- project1/blogproject/blogproject/urls.py
+++ project1/blogproject/blogproject/urls.py
@@ -5,8 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('book.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
